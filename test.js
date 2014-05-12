23e3d4403493216307fe27a1b7a87d39dafc9fc2
Jason McGhee <mcghee.j@berkeley.edu>
2014-05-11 20:55:19 -0700

    refactored method of increasing precision

--- test.js
+++ test.js
@@ -25,8 +25,8 @@ function percentChange(newSize, oldSize) {
 	
 	var sign;
 	var change;
-	var newOverOld = ((newSize)/(oldSize));
-	var percentOfOld = (Math.floor(newOverOld*1000)/1000);
+	var newOverOld = ((newSize*100)/(oldSize));
+	var percentOfOld = Math.round(newOverOld)/100;
 	change = percentOfOld - 100;
 	sign = change < 0 ? "-" : "+";
 	return sign+Math.abs(change)+"%";
